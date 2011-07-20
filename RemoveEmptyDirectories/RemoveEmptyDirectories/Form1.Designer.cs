db57546c44f3a57234c0fa061168c00fa8161bb2
Gustav <sir.gustav.the.coder@gmail.com>
2011-07-20 07:57:32 +0000

    made RemoveEmptyDir.. into a suupertool to show hidden files
    git-svn-id: svn://localhost/home/gustav/Downloads/pgnet/svn/trunk@44 66b2bdb8-0ade-450a-8434-a11202624ca9

--- RemoveEmptyDirectories/RemoveEmptyDirectories/Form1.Designer.cs
+++ RemoveEmptyDirectories/RemoveEmptyDirectories/Form1.Designer.cs
@@ -28,65 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dGo = new System.Windows.Forms.Button();
-            this.dOutput = new System.Windows.Forms.TextBox();
-            this.dInput = new System.Windows.Forms.TextBox();
-            this.dWork = new System.ComponentModel.BackgroundWorker();
-            this.SuspendLayout();
-            // 
-            // dGo
-            // 
-            this.dGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.dGo.Location = new System.Drawing.Point(205, 12);
-            this.dGo.Name = "dGo";
-            this.dGo.Size = new System.Drawing.Size(75, 23);
-            this.dGo.TabIndex = 0;
-            this.dGo.Text = "Go!";
-            this.dGo.UseVisualStyleBackColor = true;
-            this.dGo.Click += new System.EventHandler(this.dGo_Click);
-            // 
-            // dOutput
-            // 
-            this.dOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.dOutput.Location = new System.Drawing.Point(12, 40);
-            this.dOutput.Multiline = true;
-            this.dOutput.Name = "dOutput";
-            this.dOutput.ReadOnly = true;
-            this.dOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.dOutput.Size = new System.Drawing.Size(268, 213);
-            this.dOutput.TabIndex = 1;
-            this.dOutput.WordWrap = false;
-            // 
-            // dInput
-            // 
-            this.dInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.dInput.Location = new System.Drawing.Point(12, 14);
-            this.dInput.Name = "dInput";
-            this.dInput.Size = new System.Drawing.Size(187, 20);
-            this.dInput.TabIndex = 2;
-            // 
-            // dWork
-            // 
-            this.dWork.WorkerReportsProgress = true;
-            this.dWork.DoWork += new System.ComponentModel.DoWorkEventHandler(this.dWork_DoWork);
-            this.dWork.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.dWork_RunWorkerCompleted);
-            this.dWork.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.dWork_ProgressChanged);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 265);
-            this.Controls.Add(this.dInput);
-            this.Controls.Add(this.dOutput);
-            this.Controls.Add(this.dGo);
-            this.Name = "Form1";
-            this.Text = "Remove Empty Directories";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.dGo = new System.Windows.Forms.Button();
+			this.dOutput = new System.Windows.Forms.TextBox();
+			this.dInput = new System.Windows.Forms.TextBox();
+			this.dWork = new System.ComponentModel.BackgroundWorker();
+			this.dShow = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// dGo
+			// 
+			this.dGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.dGo.Location = new System.Drawing.Point(292, 11);
+			this.dGo.Name = "dGo";
+			this.dGo.Size = new System.Drawing.Size(75, 23);
+			this.dGo.TabIndex = 0;
+			this.dGo.Text = "Remove!";
+			this.dGo.UseVisualStyleBackColor = true;
+			this.dGo.Click += new System.EventHandler(this.dGo_Click);
+			// 
+			// dOutput
+			// 
+			this.dOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.dOutput.Location = new System.Drawing.Point(12, 40);
+			this.dOutput.Multiline = true;
+			this.dOutput.Name = "dOutput";
+			this.dOutput.ReadOnly = true;
+			this.dOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.dOutput.Size = new System.Drawing.Size(425, 213);
+			this.dOutput.TabIndex = 1;
+			this.dOutput.WordWrap = false;
+			// 
+			// dInput
+			// 
+			this.dInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.dInput.Location = new System.Drawing.Point(12, 13);
+			this.dInput.Name = "dInput";
+			this.dInput.Size = new System.Drawing.Size(274, 20);
+			this.dInput.TabIndex = 2;
+			// 
+			// dWork
+			// 
+			this.dWork.WorkerReportsProgress = true;
+			this.dWork.DoWork += new System.ComponentModel.DoWorkEventHandler(this.dWork_DoWork);
+			this.dWork.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.dWork_RunWorkerCompleted);
+			this.dWork.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.dWork_ProgressChanged);
+			// 
+			// dShow
+			// 
+			this.dShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.dShow.Location = new System.Drawing.Point(373, 11);
+			this.dShow.Name = "dShow";
+			this.dShow.Size = new System.Drawing.Size(64, 23);
+			this.dShow.TabIndex = 3;
+			this.dShow.Text = "Show!";
+			this.dShow.UseVisualStyleBackColor = true;
+			this.dShow.Click += new System.EventHandler(this.dShow_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(449, 265);
+			this.Controls.Add(this.dShow);
+			this.Controls.Add(this.dInput);
+			this.Controls.Add(this.dOutput);
+			this.Controls.Add(this.dGo);
+			this.Name = "Form1";
+			this.Text = "Dir Operator!";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -96,6 +109,7 @@
         private System.Windows.Forms.TextBox dOutput;
         private System.Windows.Forms.TextBox dInput;
         private System.ComponentModel.BackgroundWorker dWork;
+		private System.Windows.Forms.Button dShow;
     }
 }
 
--- RemoveEmptyDirectories/RemoveEmptyDirectories/Form1.cs
+++ RemoveEmptyDirectories/RemoveEmptyDirectories/Form1.cs
@@ -10,136 +10,179 @@ using System.IO;
 
 namespace RemoveEmptyDirectories
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void dWork_DoWork(object sender, DoWorkEventArgs e)
-        {
-            var source = (string)e.Argument;
-            mess("started on " + source);
+		enum Type { Remove, Show }
 
-            work(source);
+		struct Input
+		{
+			public string dir;
+			public Type type;
+		}
 
-            mess("done");
-        }
+		private void dWork_DoWork(object sender, DoWorkEventArgs e)
+		{
+			var source = (Input)e.Argument;
+			mess("started on " + source.dir);
 
-        private void work(string dir)
-        {
-            if (false == Directory.Exists(dir))
-            {
-                mess(dir + " doesnt exist");
-                return;
-            }
+			if (source.type == Type.Remove)
+			{
+				removeDirectoriesRec(source.dir);
+			}
+			else
+			{
+				showHiddenFiles(source.dir);
+			}
 
-            List<string> dirs = new List<string>(Directory.GetDirectories(dir));
-            foreach (var sub in dirs)
-            {
-                work(sub);
-            }
+			mess("done");
+		}
 
-            var files = Directory.GetFiles(dir);
-            if (files.Count() > 0)
-            {
-                List<string> validfiles = new List<string>();
-                foreach (var f in files)
-                {
-                    if (IsUselessFile(f))
-                    {
-                        mess("Deleting " + f);
-                        try
-                        {
-                            File.Delete(f);
-                        }
-                        catch (Exception e)
-                        {
-                            mess(e.Message);
-                        }
-                    }
-                    else
-                    {
-                        if (IsStrangeFile(f))
-                        {
-                            validfiles.Add(f);
-                        }
-                    }
-                }
+		private void showHiddenFiles(string dir)
+		{
+			foreach (var d in Directory.GetDirectories(dir))
+			{
+				showHiddenFiles(d);
+			}
 
-                if (validfiles.Count < 10)
-                {
-                    foreach (var f in validfiles)
-                    {
-                        mess("   " + f + ": " + new FileInfo(f).Attributes.ToString());
-                    }
-                }
-            }
+			foreach (var f in Directory.GetFiles(dir))
+			{
+				var fi = new FileInfo(f);
+				if ((fi.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+				{
+					mess("Setting " + f + " from " + fi.Attributes.ToString() + " to normal");
+					fi.Attributes = FileAttributes.Normal;
+				}
+			}
+		}
 
-            // contains non-empty subdirectories...
-            if( Directory.GetDirectories(dir).Count() > 0 ) return;
+		private void removeDirectoriesRec(string dir)
+		{
+			if (false == Directory.Exists(dir))
+			{
+				mess(dir + " doesnt exist");
+				return;
+			}
 
-            // contains files
-            if( Directory.GetFiles(dir).Count() > 0 ) return;
+			List<string> dirs = new List<string>(Directory.GetDirectories(dir));
+			foreach (var sub in dirs)
+			{
+				removeDirectoriesRec(sub);
+			}
 
-            try
-            {
-                mess(dir + " removing...");
-                var di = new DirectoryInfo(dir);
-                di.Attributes = FileAttributes.Normal;
-                
-                Directory.Delete(dir);
-            }
-            catch (Exception e)
-            {
-                mess("ERROR: " + dir + " was not removed, " + e.Message);
-                return;
-            }
+			var files = Directory.GetFiles(dir);
+			if (files.Count() > 0)
+			{
+				List<string> validfiles = new List<string>();
+				foreach (var f in files)
+				{
+					if (IsUselessFile(f))
+					{
+						mess("Deleting " + f);
+						try
+						{
+							File.Delete(f);
+						}
+						catch (Exception e)
+						{
+							mess(e.Message);
+						}
+					}
+					else
+					{
+						if (IsStrangeFile(f))
+						{
+							validfiles.Add(f);
+						}
+					}
+				}
 
-            mess(dir + " removed!");
-        }
+				if (validfiles.Count < 10)
+				{
+					foreach (var f in validfiles)
+					{
+						mess("   " + f + ": " + new FileInfo(f).Attributes.ToString());
+					}
+				}
+			}
 
-        private bool IsUselessFile(string filename)
-        {
-            var f = filename.ToLower();
-            if (f.EndsWith("thumbs.db")) return true;
-            if (f.EndsWith("desktop.ini")) return true;
-            return false;
-        }
+			// contains non-empty subdirectories...
+			if (Directory.GetDirectories(dir).Count() > 0) return;
 
-        private bool IsStrangeFile(string filename)
-        {
-            var f = filename.ToLower();
-            if (f.EndsWith(".mp3")) return false;
-            if (f.EndsWith(".mod")) return false;
-            if (f.EndsWith(".ogg")) return false;
-            if (f.EndsWith(".sid")) return false;
-            return true;
-        }
+			// contains files
+			if (Directory.GetFiles(dir).Count() > 0) return;
 
-        private void mess(string m)
-        {
-            dWork.ReportProgress(0, m);
-        }
+			try
+			{
+				mess(dir + " removing...");
+				var di = new DirectoryInfo(dir);
+				di.Attributes = FileAttributes.Normal;
 
-        private void dGo_Click(object sender, EventArgs e)
-        {
-            dGo.Enabled = false;
-            dInput.Enabled = false;
-            dOutput.Text = "";
-            dWork.RunWorkerAsync(dInput.Text);
-        }
+				Directory.Delete(dir);
+			}
+			catch (Exception e)
+			{
+				mess("ERROR: " + dir + " was not removed, " + e.Message);
+				return;
+			}
 
-        private void dWork_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-            dOutput.AppendText((string)e.UserState + Environment.NewLine);
-        }
+			mess(dir + " removed!");
+		}
 
-        private void dWork_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            dGo.Enabled = true;
-            dInput.Enabled = true;
-        }
-    }
+		private bool IsUselessFile(string filename)
+		{
+			var f = filename.ToLower();
+			if (f.EndsWith("thumbs.db")) return true;
+			if (f.EndsWith("desktop.ini")) return true;
+			return false;
+		}
+
+		private bool IsStrangeFile(string filename)
+		{
+			var f = filename.ToLower();
+			if (f.EndsWith(".mp3")) return false;
+			if (f.EndsWith(".mod")) return false;
+			if (f.EndsWith(".ogg")) return false;
+			if (f.EndsWith(".sid")) return false;
+			return true;
+		}
+
+		private void mess(string m)
+		{
+			dWork.ReportProgress(0, m);
+		}
+
+		private void dGo_Click(object sender, EventArgs e)
+		{
+			startAction(Type.Remove);
+		}
+
+		private void startAction(Type remove)
+		{
+			dGo.Enabled = false;
+			dInput.Enabled = false;
+			dOutput.Text = "";
+			dWork.RunWorkerAsync(new Input { dir = dInput.Text, type = remove });
+		}
+
+		private void dWork_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			dOutput.AppendText((string)e.UserState + Environment.NewLine);
+		}
+
+		private void dWork_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			dGo.Enabled = true;
+			dInput.Enabled = true;
+		}
+
+		private void dShow_Click(object sender, EventArgs e)
+		{
+			startAction(Type.Show);
+		}
+	}
 }
